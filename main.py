e164d9991b2be764c462b4a064070893e0b6b570
Seppe <r0881427@student.thomasmore.be>
2023-10-05 18:10:40 +0200

    check for empty inputs

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ operator = st.radio("Select the operator", ('\+', '\-', '\*', '\/'))[1]
 solution = st.text_input("Enter the solution").lower()
 
 if (st.button("Solve", type="primary")):
+    if not area or not solution:
+        st.error(f"Do not leave the {'words' if not area else 'solution'} empty")
+        st.stop()
+
     if len(set(solution + ''.join(factors))) > 10:
         st.error("Solution and words can only contain up to 10 unique letters")
         st.stop()
 
     if len(max(factors, key=len)) > 5 or len(solution) > 5:
         st.error(f"{'Each of the words' if not len(solution) > 5 else 'The solution'} shouldn't be longer than 5 letters")
-        st.stop()
-
-    if not area or not solution:
-        st.error(f"Do not leave the {'words' if not area else 'solution'} empty")
         st.stop()
 
     st.markdown("---")
